d0583db2772b865eaca28308d9580aa8a4d92e0e
benrobo <alumonabenaiah71@gmail.com>
2023-04-30 21:13:24 +0100

    completed thread bot service

--- packages/bot/app.ts
+++ packages/bot/app.ts
@@ -46,6 +46,38 @@ client.on("interactionCreate", async (interaction) => {
       interaction.reply({ embeds: [embeddMsg], ephemeral: true });
   }
   if (commandName === "threads") {
+    try {
+      const response = await botServices.handleThreads(channelId);
+      const embeddColor = response?.success ? 0x3f7eee : 0xff0000;
+      const embeddTitle = response?.success
+        ? `✅ **Latest Thread**`
+        : `❌ **Failed Fetching Thread**`;
+
+      let embeddMsg = embed
+        .setTitle(embeddTitle)
+        .setDescription(response?.content ?? response?.msg)
+        .setColor(embeddColor)
+        .setURL(response?.url)
+        .setImage(
+          response?.image ??
+            "https://images-ext-1.discordapp.net/external/qzgQmLYic48-UPuxj52aRYm9vXgpjvUoXqNXPUvwWxE/https/assets.showwcase.com/og-image/default.png?width=1382&height=972"
+        );
+
+      // * ephemeral: true would only make the message visible to sender.
+      if (response?.success === false)
+        interaction.reply({ embeds: [embeddMsg], ephemeral: true });
+      if (response?.success === true)
+        interaction.reply({
+          embeds: [embeddMsg],
+          ephemeral: false,
+        });
+    } catch (e: any) {
+      // console.log(e);
+      const embeddMsg = embed
+        .setTitle(`❌ **Failed Fetching Thread**`)
+        .setDescription(`Something went wrong. Please try again later.`);
+      interaction.reply({ embeds: [embeddMsg], ephemeral: true });
+    }
   }
 });
 
