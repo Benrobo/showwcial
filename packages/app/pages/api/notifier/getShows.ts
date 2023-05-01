89c03a8ccb6d40058d41563b76c13dec518f6c5f
benrobo <alumonabenaiah71@gmail.com>
2023-05-01 11:51:39 +0100

    completed shows implementation

---
+++ packages/app/pages/api/notifier/getShows.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { CatchErrors } from "../middlewares/catchError";
+// import { isLoggedIn } from "../middlewares/auth";
+import NotifierController from "../controllers/notifier";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const notifier = new NotifierController();
+
+  if (req.method === "POST") {
+    await notifier.fetchShows(req, res);
+  }
+}
+
+export default CatchErrors(handler);
