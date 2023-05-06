8b4e2b7fc582eeb30067424de4ce35e803e45651
benrobo <alumonabenaiah71@gmail.com>
2023-05-06 16:04:06 +0100

    added major changes

--- packages/app/pages/api/controllers/pageBuilder.ts
+++ packages/app/pages/api/controllers/pageBuilder.ts
@@ -6,6 +6,7 @@ import prisma from "../config/prisma";
 import { GetDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
 import { v4 as uuidv4 } from "uuid";
 import { SiteSchema } from "../helper/validator";
+import axios from "axios";
 
 function notion(token: string) {
   const notion = new Client({
@@ -138,6 +139,57 @@ export default class PageBuilderController extends BaseController {
     );
   }
 
+  public async fetchUserDetails(username: string) {
+    let response = {
+      about: null,
+      experiences: null,
+      stacks: null,
+      repo: null,
+      socials: null,
+      resumeUrl: null,
+    };
+    try {
+      const userData = await axios
+        .get(`https://cache.showwcase.com/user/${username}`)
+        .then((r) => r?.data);
+      const experiences = await axios
+        .get(`https://cache.showwcase.com/user/${username}/experiences`)
+        .then((r) => r?.data);
+      const stacks = await axios
+        .get(`https://cache.showwcase.com/user/${username}/stacks`)
+        .then((r) => r?.data);
+      const ghRepo = await axios
+        .get(`https://cache.showwcase.com/user/${username}/github_repos`)
+        .then((r) => r?.data);
+      const socials = await axios
+        .get(`https://cache.showwcase.com/user/${username}/socials`)
+        .then((r) => r?.data);
+
+      const result = Promise.all([
+        userData,
+        experiences,
+        stacks,
+        ghRepo,
+        socials,
+      ]).then((res) => {
+        const [userData, experiences, stacks, ghRepo, socials] = res;
+        response["about"] = userData?.about;
+        response["resumeUrl"] = userData?.resumeUrl;
+        response["experiences"] = experiences;
+        response["stacks"] =
+          stacks?.length > 0 ? stacks?.map((d) => d?.stack?.name) : [];
+        response["repo"] = ghRepo;
+        response["socials"] = socials?.links;
+        return response;
+      });
+
+      return result;
+    } catch (e: any) {
+      console.log(`Error: ${e}`);
+      return response;
+    }
+  }
+
   public async createSite(req: NextApiRequest, res: NextApiResponse) {
     const payload = req.body;
     const uId = req["user"]?.id;
@@ -161,19 +213,128 @@ export default class PageBuilderController extends BaseController {
     // check if notion Id exits
     const notionExists = await prisma.site.findFirst({
       where: { notionDatabaseId: notionPageId },
+      include: { user: true },
     });
+
     if (notionExists !== null) {
       // ! fetch user showwcase social links, experiences,
+      const { about, experiences, repo, resumeUrl, socials, stacks } =
+        await this.fetchUserDetails(notionExists?.user?.username);
+
+      const githubRepoData = [];
+      const socialLinks = [];
+      const formatedExperiences = [];
+
+      if (repo !== null) {
+        repo.forEach((rp) => {
+          if (rp?.pinned) {
+            const repoInfo = {
+              name: rp?.name,
+              description: rp?.description,
+              url: rp?.htmlUrl,
+              tags: Object.keys(rp?.languages),
+            };
+            githubRepoData.push(repoInfo);
+          }
+        });
+      }
+      if (socials !== null && socials?.length > 0) {
+        socials.forEach((s) => {
+          let links = {
+            label: s?.label,
+            url: s?.value,
+          };
+          socialLinks.push(links);
+        });
+      }
+      if (experiences !== null && experiences?.length > 0) {
+        experiences.forEach((e) => {
+          let exp = {
+            title: e?.title,
+            companyName: e?.companyName,
+            startDate: e?.startDate,
+            endDate: e?.endDate,
+            current: e?.current,
+            description: e?.description,
+          };
+          formatedExperiences.push(exp);
+        });
+      }
 
       // update site
-      await prisma.site.update({
+      const createdSite = await prisma.site.update({
         where: { id: notionExists?.id },
         data: {
           name,
           themeName,
           notionDatabaseId: notionPageId,
+          slug,
+          pageType: type,
+          id: uuidv4(),
+          userId: uId,
+          portfolioData: {
+            create: {
+              id: uuidv4(),
+              experiences: JSON.stringify(formatedExperiences),
+              headline: "I solve problem for a living.",
+              socialLinks: JSON.stringify(socialLinks),
+              tagline:
+                "Mission driven software engineer, with a passion for thoughtful UI design, collaboration, and teaching.",
+              about,
+              email: notionExists?.user?.email,
+              resumeUrl,
+              stacks,
+              ghRepo: JSON.stringify(githubRepoData),
+            },
+          },
         },
       });
+
+      this.success(
+        res,
+        "--pageBuilder/success",
+        "Site created successfully.",
+        200
+      );
+      return;
     }
+
+    this.error(
+      res,
+      "--pageBuilder/verify-notion-page",
+      "Please verify notion page first.",
+      400
+    );
+  }
+
+  public async getCreatedSites(req: NextApiRequest, res: NextApiResponse) {
+    const uId = req["user"]?.id;
+    const createdSites = await prisma.site.findMany({
+      where: { userId: uId },
+      include: { portfolioData: true },
+    });
+
+    createdSites.forEach((d) => {
+      if (d?.portfolioData !== null) {
+        const portfolio = d?.portfolioData;
+        if (typeof portfolio?.ghRepo === "string") {
+          d.portfolioData["ghRepo"] = JSON.parse(portfolio?.ghRepo);
+        }
+        if (typeof portfolio?.experiences === "string") {
+          d.portfolioData["experiences"] = JSON.parse(portfolio?.experiences);
+        }
+        if (typeof portfolio?.socialLinks === "string") {
+          d.portfolioData["socialLinks"] = JSON.parse(portfolio?.socialLinks);
+        }
+      }
+    });
+
+    this.success(
+      res,
+      "--pageBuilder/success",
+      "sites fetched successfully.",
+      200,
+      { sites: createdSites }
+    );
   }
 }
