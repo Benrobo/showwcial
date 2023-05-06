789b9b49c4f5f1733aee70c2bb6dad4832427147
benrobo <alumonabenaiah71@gmail.com>
2023-05-06 06:28:06 +0100

    added settings and notion integration

--- packages/app/pages/api/config/index.ts
+++ packages/app/pages/api/config/index.ts
@@ -1,6 +1,3 @@
-import { Client } from "@notionhq/client";
 import ENV from "./env";
 
 export const CLIENT_URL = "http://localhost:3000";
-
-export const notion = new Client({ auth: ENV.notionApiToken });
