6e1d12fb9bac507f1732ff6823f08f7788b334bc
benrobo <alumonabenaiah71@gmail.com>
2023-05-09 17:19:09 +0100

    added page tracking feature

---
+++ packages/app/pages/api/pageBuilder/getViews/[slug].ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { CatchErrors } from "../../middlewares/catchError";
+import PageBuilderController from "../../controllers/pageBuilder";
+import TrackVisitor from "../../middlewares/trackVisitors";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const pageBuilder = new PageBuilderController();
+
+  if (req.method === "GET") {
+    await pageBuilder.getPageViews(req, res);
+  }
+}
+
+export default CatchErrors(handler);
