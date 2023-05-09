7959bb55d6ddb9d183b7da47f787a10557cee3c8
benrobo <alumonabenaiah71@gmail.com>
2023-05-09 23:17:01 +0100

    quit using agenda for registering background jobs

--- packages/app/pages/api/helper/sendMail.ts
+++ packages/app/pages/api/helper/sendMail.ts
@@ -1,77 +1,79 @@
-import env from "../config/env";
-import nodemailer from "nodemailer";
-import agendaConfig from "../config/agenda";
+// ! this was commented out due to agenda reliability of connecting to mongodb server.
 
-const mailConfig = {
-  host: env.emailHost,
-  port: env.emailPort,
-  auth: {
-    user: env.emailUser,
-    pass: env.emailPassword,
-  },
-};
+// import env from "../config/env";
+// import nodemailer from "nodemailer";
+// import agendaConfig from "../config/agenda";
 
-const agenda = agendaConfig;
+// const mailConfig = {
+//   host: env.emailHost,
+//   port: env.emailPort,
+//   auth: {
+//     user: env.emailUser,
+//     pass: env.emailPassword,
+//   },
+// };
 
-// init job queue using agenda
-agenda.define("send_email", { concurrency: 1 }, async (job, done) => {
-  try {
-    const transporter = nodemailer.createTransport(mailConfig);
-    const { to, subject, body, from }: any = job.attrs.data;
-    const mailOptions = {
-      from: env.emailSender,
-      to,
-      subject,
-      html: body,
-    };
-    await transporter.sendMail(mailOptions);
-    console.log(`Mail sent successfully. ${to}`);
-    done();
-  } catch (e: any) {
-    console.error(`Failed sending mail: ${e}`);
-    done();
-  }
-});
+// const agenda = agendaConfig;
 
-// schedule welcome mail
-agenda.define("send_welcome_mail", { concurrency: 1 }, async (job, done) => {
-  try {
-    const transporter = nodemailer.createTransport(mailConfig);
-    const { to, subject, body, from }: any = job.attrs.data;
-    const mailOptions = {
-      from: env.emailSender,
-      to,
-      subject,
-      html: body,
-    };
-    await transporter.sendMail(mailOptions);
-    console.info(`Mail sent successfully. ${to}`);
-    done();
-  } catch (e: any) {
-    console.info(`Failed sending welcome mail: ${e.message}`);
-    done();
-  }
-});
+// // init job queue using agenda
+// agenda.define("send_email", { concurrency: 1 }, async (job, done) => {
+//   try {
+//     const transporter = nodemailer.createTransport(mailConfig);
+//     const { to, subject, body, from }: any = job.attrs.data;
+//     const mailOptions = {
+//       from: env.emailSender,
+//       to,
+//       subject,
+//       html: body,
+//     };
+//     await transporter.sendMail(mailOptions);
+//     console.log(`Mail sent successfully. ${to}`);
+//     done();
+//   } catch (e: any) {
+//     console.error(`Failed sending mail: ${e}`);
+//     done();
+//   }
+// });
 
-(async function () {
-  // start the agenda server
-  await agenda.start();
-})();
+// // schedule welcome mail
+// agenda.define("send_welcome_mail", { concurrency: 1 }, async (job, done) => {
+//   try {
+//     const transporter = nodemailer.createTransport(mailConfig);
+//     const { to, subject, body, from }: any = job.attrs.data;
+//     const mailOptions = {
+//       from: env.emailSender,
+//       to,
+//       subject,
+//       html: body,
+//     };
+//     await transporter.sendMail(mailOptions);
+//     console.info(`Mail sent successfully. ${to}`);
+//     done();
+//   } catch (e: any) {
+//     console.info(`Failed sending welcome mail: ${e.message}`);
+//     done();
+//   }
+// });
 
-export async function sendMail(to: string, subject: string, body: string) {
-  agenda.schedule(new Date(), "send_email", { to, subject, body });
-  agenda.start();
-}
+// (async function () {
+//   // start the agenda server
+//   await agenda.start();
+// })();
 
-export async function sendWelcomeMail(
-  to: string,
-  subject: string,
-  body: string
-) {
-  agenda.schedule("in 1 minutes", "send_welcome_mail", {
-    to,
-    subject,
-    body,
-  });
-  agenda.start();
-}
+// export async function sendMail(to: string, subject: string, body: string) {
+//   agenda.schedule(new Date(), "send_email", { to, subject, body });
+//   agenda.start();
+// }
+
+// export async function sendWelcomeMail(
+//   to: string,
+//   subject: string,
+//   body: string
+// ) {
+//   agenda.schedule("in 1 minutes", "send_welcome_mail", {
+//     to,
+//     subject,
+//     body,
+//   });
+//   agenda.start();
+// }
