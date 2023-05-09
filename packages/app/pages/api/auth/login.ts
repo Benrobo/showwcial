28ec605c046308f5b42af66f9842f323214d5b35
benrobo <alumonabenaiah71@gmail.com>
2023-05-09 23:16:50 +0100

    quit using agenda for registering background jobs

--- packages/app/pages/api/auth/login.ts
+++ packages/app/pages/api/auth/login.ts
@@ -1,15 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { CatchErrors } from "../middlewares/catchError";
 import Authentication from "../controllers/auth";
-import agendaConfig from "../config/agenda";
+// import agendaConfig from "../config/agenda";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // try connecting agenda to mongodb before loading the controller method.
-  agendaConfig
-    .start()
-    .then(() => console.log("Agenda connected to MongoDB"))
-    .catch((err) => console.log(`Agenda failed to connect to MongoDB: ${err}`));
-
   const authentication = new Authentication();
 
   if (req.method === "POST") {
