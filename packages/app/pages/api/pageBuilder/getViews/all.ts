ce16110786b855b57c5139ffada0e5d68bd26d89
benrobo <alumonabenaiah71@gmail.com>
2023-05-09 22:51:05 +0100

    added page tracking

---
+++ packages/app/pages/api/pageBuilder/getViews/all.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { CatchErrors } from "../../middlewares/catchError";
+import PageBuilderController from "../../controllers/pageBuilder";
+import { isLoggedIn } from "../../middlewares/auth";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const pageBuilder = new PageBuilderController();
+
+  if (req.method === "GET") {
+    await pageBuilder.getAllSitesViews(req, res);
+  }
+}
+
+export default isLoggedIn(CatchErrors(handler));
