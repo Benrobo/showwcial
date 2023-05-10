0aea3c5880fb36da3d2873bc7da2336f3c1af6e1
benrobo <alumonabenaiah71@gmail.com>
2023-05-10 12:35:25 +0100

    updated settings credentials integration

--- packages/app/pages/api/settings/addToken.ts
+++ packages/app/pages/api/settings/addToken.ts
@@ -3,10 +3,18 @@ import { CatchErrors } from "../middlewares/catchError";
 import { isLoggedIn } from "../middlewares/auth";
 import SettingsController from "../controllers/settings";
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "5mb",
+    },
+  },
+};
+
 function handler(req: NextApiRequest, res: NextApiResponse) {
   const settings = new SettingsController();
   if (req.method === "POST") {
-    settings.addNotionToken(req, res);
+    settings.addExternalCredentials(req, res);
   }
 }
 
