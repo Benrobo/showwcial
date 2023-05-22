99770ce9f2fabc0e8790a839204b1fd544ee1a4c
benrobo <alumonabenaiah71@gmail.com>
2023-05-22 22:35:51 +0100

    added error banner for discord bot status

--- packages/app/components/Layout/mainDashboard.tsx
+++ packages/app/components/Layout/mainDashboard.tsx
@@ -1,6 +1,7 @@
 import next from "next";
 import SideBar from "../Navbar/sidebar";
 import TopBar from "../Navbar/topbar";
+import ErrorBanner from "../../components/ErrorBanner";
 
 interface DashboardLayoutProp {
   children: React.ReactNode;
@@ -10,6 +11,7 @@ interface DashboardLayoutProp {
 function MainDashboardLayout({ children, activeTab }: DashboardLayoutProp) {
   return (
     <div className="w-full h-full">
+      <ErrorBanner />
       <TopBar />
       <div className="w-full h-full py-9 flex items-start justify-start overflowHidden">
         <SideBar active={activeTab} />
