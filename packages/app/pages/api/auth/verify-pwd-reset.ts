bb34bafc963413e28f0708f0b5fa6170ee1a4895
benrobo <alumonabenaiah71@gmail.com>
2023-05-23 18:30:26 +0100

    started implementing forget password .

---
+++ packages/app/pages/api/auth/verify-pwd-reset.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { CatchErrors } from "../middlewares/catchError";
+import Authentication from "../controllers/auth";
+// import agendaConfig from "../config/agenda";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const authentication = new Authentication();
+
+  if (req.method === "POST") {
+    await authentication.verifyPwdReset(req, res);
+  }
+}
+
+export default CatchErrors(handler);
