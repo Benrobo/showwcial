a4d1df4bef027304b4f888b73ccfaf108f496dcc
benrobo <alumonabenaiah71@gmail.com>
2023-06-08 18:02:20 +0100

    added major update

--- packages/app/pages/api/controllers/friends.ts
+++ packages/app/pages/api/controllers/friends.ts
@@ -13,18 +13,33 @@ export default class FriendController extends BaseController {
     const userToken = await prisma.settings.findFirst({
       where: { userId: userId },
     });
+    const userData = await prisma.users.findFirst({ where: { id: userId } });
     try {
-      const res = await axios.get(
-        `https://cache.showwcase.com/users/suggested_followers`,
-        {
+      const suggFollowers = await axios
+        .get(`https://cache.showwcase.com/users/suggested_followers`, {
           headers: {
             "X-API-KEY": userToken.showwcaseToken,
           },
+        })
+        .then((res) => res.data);
+      const userFollowers = await axios
+        .get(
+          `https://cache.showwcase.com/network/followers?username=${userData.username}`,
+          {
+            headers: {
+              "X-API-KEY": userToken.showwcaseToken,
+            },
+          }
+        )
+        .then((res) => res.data);
+
+      const data = Promise.all([suggFollowers, userFollowers]).then(
+        (result) => {
+          const [suggFollowers, userFollowers] = result;
+          return { data: { suggFollowers, userFollowers }, success: true };
         }
       );
-      response["data"] = res?.data ?? (res as any)?.response?.data;
-      response["success"] = true;
-      return response;
+      return data;
     } catch (e: any) {
       console.log(e);
       response["data"] = e.response?.data ?? {
@@ -56,7 +71,38 @@ export default class FriendController extends BaseController {
     }
 
     const data = followers.data;
+    const suggFollow = data.suggFollowers;
+    const userFollowers = data.userFollowers;
 
-    console.log(data);
+    const filterUser = suggFollow.map((user, idx, arr) => {
+      if (user.id !== userFollowers[idx].id) {
+        return {
+          id: user.id,
+          username: user.username,
+          fullname: user.displayName,
+          image: user.profilePictureKey,
+          followers: user.totalFollowers,
+          tags: user.tags,
+        };
+      }
+    });
+
+    if (filterUser.length === 0) {
+      this.error(
+        res,
+        "----suggestedFollowers/something-went-wrong",
+        "No users for now",
+        404
+      );
+      return;
+    }
+
+    this.success(
+      res,
+      "--suggestedFollowers/success",
+      "suggested followers fetched.",
+      200,
+      filterUser
+    );
   }
 }
