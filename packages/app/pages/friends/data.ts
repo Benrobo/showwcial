2f78bf4bc7df2c2d93f6bf784a01dac8284781dc
benrobo <alumonabenaiah71@gmail.com>
2023-06-08 12:50:44 +0100

    feat: added tinder follow style

---
+++ packages/app/pages/friends/data.ts
@@ -0,0 +1,34 @@
+export const fakeUser = [
+  {
+    username: "johnny",
+    fullname: "John Doe",
+    userId: 121212,
+    tags: ["Dancing", "Drawing"],
+    profilePic:
+      "https://profile-assets.showwcase.com/102662/1683311191679-1683311188721-DSC_1744.jpeg",
+  },
+  {
+    username: "benny",
+    fullname: "Benny Ben",
+    userId: 12343,
+    tags: ["Dancing", "Drawing", "Feeding"],
+    profilePic:
+      "https://profile-assets.showwcase.com/102662/1683311191679-1683311188721-DSC_1744.jpeg",
+  },
+  {
+    username: "user123",
+    fullname: "John Smith",
+    userId: 98765,
+    tags: ["Reading", "Photography"],
+    profilePic:
+      "https://profile-assets.showwcase.com/102662/1683311191679-1683311188721-DSC_1744.jpeg",
+  },
+  {
+    username: "coolguy",
+    fullname: "Mike Anderson",
+    userId: 54321,
+    tags: ["Surfing", "Painting"],
+    profilePic:
+      "https://profile-assets.showwcase.com/102662/1683311191679-1683311188721-DSC_1744.jpeg",
+  },
+];
