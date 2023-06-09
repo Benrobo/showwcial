4e05527cc9c5ab63eca8e93466257f4b92f2bc35
benrobo <alumonabenaiah71@gmail.com>
2023-06-09 21:00:55 +0100

    added major update

--- packages/app/components/Navbar/sidebar.tsx
+++ packages/app/components/Navbar/sidebar.tsx
@@ -17,9 +17,11 @@ import { TbWorld } from "react-icons/tb";
 import ImageTag from "../Image";
 import { CgWebsite } from "react-icons/cg";
 import { TiPointOfInterestOutline } from "react-icons/ti";
+import { IoVideocam } from "react-icons/io5";
 
 interface SidebarProps {
   active?: string;
+  isNew?: boolean;
 }
 
 const returnActiveStyle = (active: string, name: string) => {
@@ -62,6 +64,14 @@ const returnActiveStyle = (active: string, name: string) => {
 };
 
 function SideBar({ active }: SidebarProps) {
+  const renderNewBadge = () => {
+    return (
+      <span className="absolute top-[-6px] right-[0px] px-2 py-[.9px] rounded-md text-white-100 text-[9px] bg-red-305 ">
+        New
+      </span>
+    );
+  };
+
   return (
     <div className="w-[18%] h-[100vh] py-[2em] border-r-[1px] border-r-solid border-r-white-600  overflow-y-scroll hideScrollBar">
       <ul className="w-full flex flex-col items-start justify-start px-2 gap-6 mt-7">
@@ -70,27 +80,28 @@ function SideBar({ active }: SidebarProps) {
             className={`${returnActiveStyle(
               active,
               "dashboard"
-            )} hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-start justify-start`}
+            )} relative border-solid border-[.1px] border-white-600 hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-start justify-start`}
           >
             <MdSpaceDashboard className="ml-2 text-2xl " /> Dashboard
           </li>
         </Link>
-        {/* <Link href="/chat" className="w-full">
+        <Link href="/meet" className="w-full">
           <li
             className={`${returnActiveStyle(
               active,
-              "chat"
-            )} hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
+              "meet"
+            )} relative border-solid border-[.1px] border-white-600 hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
           >
-            <MdWebStories className="ml-2 text-2xl " /> Chat
+            <IoVideocam className="ml-2 text-2xl " /> Meet
+            {renderNewBadge()}
           </li>
-        </Link> */}
+        </Link>
         <Link href="/hub" className="w-full">
           <li
             className={`${returnActiveStyle(
               active,
               "hub"
-            )} hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
+            )} relative border-solid border-[.1px] border-white-600 hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
           >
             <MdWebStories className="ml-2 text-2xl " /> Hub
           </li>
@@ -100,7 +111,7 @@ function SideBar({ active }: SidebarProps) {
             className={`${returnActiveStyle(
               active,
               "notifier"
-            )} hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
+            )} relative border-solid border-[.1px] border-white-600 hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
           >
             <BsRobot className="ml-2 text-2xl " /> Notifier
           </li>
@@ -110,7 +121,7 @@ function SideBar({ active }: SidebarProps) {
             className={`${returnActiveStyle(
               active,
               "pageBuilder"
-            )} hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
+            )} relative border-solid border-[.1px] border-white-600 hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
           >
             <CgWebsite className="ml-2 text-2xl " /> Page Builder
           </li>
@@ -120,9 +131,10 @@ function SideBar({ active }: SidebarProps) {
             className={`${returnActiveStyle(
               active,
               "friends"
-            )} hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
+            )} relative border-solid border-[.1px] border-white-600 hover:bg-white-600 py-2 rounded-[10px] cursor-pointer hover:text-white-100 hover:pp-SB text-[14px] transition-all gap-3 flex items-center justify-start`}
           >
             <TiPointOfInterestOutline className="ml-2 text-2xl " /> Friendcord
+            {renderNewBadge()}
           </li>
         </Link>
 
